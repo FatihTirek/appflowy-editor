16cd3170d03f979bce40b1713d698e32c2bf9713
FatihTirek <fatih.tirek.business@gmail.com>
2026-09-19 15:46:01 +0300

    fix: remove duplicate onFocusReceived in delta_input_service

--- lib/src/editor/editor_component/service/ime/delta_input_service.dart
+++ lib/src/editor/editor_component/service/ime/delta_input_service.dart
@@ -143,8 +143,6 @@ class DeltaTextInputService extends TextInputService with DeltaTextInputClient {
     TextInputControl? newControl,
   ) {}
 
-  bool onFocusReceived() => false;
-
   @override
   void performSelector(String selectorName) {
     final currentTextEditingValue = this.currentTextEditingValue;
